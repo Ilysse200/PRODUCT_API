cf6c7a2dbbd1a6009d24f8674d9d82cc4c5f9a82
Ilysse200 <ilysseted500@gmail.com>
2025-02-27 09:30:28 +0200

    added the blog Model

---
+++ models/blogModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const{model, Schema} = mongoose
+
+const blogSchema = new Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    blogImage:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default: Date.now,
+        required:true
+    }
+
+})
