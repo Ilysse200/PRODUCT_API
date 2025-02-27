4c662b6bcf2626818a6566e14ca7e8018de27b6c
Ilysse200 <ilysseted500@gmail.com>
2025-02-27 13:05:20 +0200

    added aurhorization

--- middleware/roleIdentification.js
+++ middleware/roleIdentification.js
@@ -1,8 +1,8 @@
 import User from '../models/userModel.js'
 
 export const Admin = (req, res, next)=>{
-    if(req.User.userRole!=="admin"){
-        return res.status(400).json({message:"Access denied contact admin please!"});
+    if(req.userRole!=="admin"){
+        return res.status(403).json({message:"Access denied contact admin please!"});
     }
     next();
 };
