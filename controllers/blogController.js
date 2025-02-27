319d0ffbab699bf11f01c42a88628cb58f96e57d
Ilysse200 <ilysseted500@gmail.com>
2025-02-27 16:31:14 +0200

    modified Blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -10,15 +10,7 @@ export const CreateBlog = async (req, res) => {
         const blogUpload = await cloudinary.uploader.upload(req.file.path)
 
         const {title, description, date} = req.body;
-        if (!title || !description || !blogImage) {
-            return res.status(400).json({ message: 'All fields are required' });
-        }
-        // ✅ Ensure the date is correctly formatted
-        let parsedDate = date ? new Date(date) : new Date();
-        if (isNaN(parsedDate.getTime())) {
-            return res.status(400).json({ message: 'Invalid date format. Use YYYY-MM-DD' });
-        }
-        const blog = new blog({title,description,date:parsedDate, blogImage:blogUpload.secure_url})
+        const blog = new Blog({title,description,date, blogImage:blogUpload.secure_url})
         await blog.save();
         res.status(201).json({success:true, message:"Blog created successfully"})
         
