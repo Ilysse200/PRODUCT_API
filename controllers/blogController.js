7c557ca68607d74e10a1416eb0ebc6d2934a1b5b
Ilysse200 <ilysseted500@gmail.com>
2025-02-28 09:35:17 +0200

    added fetch a particular blog and update a blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -20,11 +20,48 @@ export const CreateBlog = async (req, res) => {
 }
 export const getAllBlogs=async(req, res)=>{
     try{
-        const B = await Product.find();
-        res.status(200).json({success:true, Products});
+        const blog = await Blog.find();
+        res.status(200).json({success:true, blog});
     }
     catch(error){
         res.status(500).json({success:false,message:"Server error", error:error.message });
 
+    }
+}
+export const getBlogsById=async(req, res)=>{
+    try{
+        const{id}=req.params;
+        const blog = await Blog.findById(id);
+        if(!blog){
+            return res.status(404).json({success:false, message:"Blog not found"})
+        }
+        res.status(200).json({success:true, blog})
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+}
+
+export const deleteBlogsById=async(req, res)=>{
+    try{
+        const{id}=req.params;
+        const blog = await Blog.findByIdAndDelete(id);
+        if(!blog){
+            return res.status(404).json({success:false, message:"Blog not found"})
+        }
+        res.status(200).json({success:true, message:"Blog deleted"})
+    }catch(error){
+        res.status(500).json({error:error.message})
+    }
+}
+export const updateBlogs=async(req, res)=>{
+    try{
+        const{id}=req.params;
+        const blog = await Blog.findByIdAndUpdate(id, req.body);
+        if(!blog){
+            return res.status(404).json({success:false, message:"Blog not found"})
+        }
+        res.status(200).json({success:true, message:"Blog updated successfully"})
+    }catch(error){
+        res.status(500).json({error:error.message})
     }
 }
--- routers/blogPath.js
+++ routers/blogPath.js
@@ -1,7 +1,11 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import {CreateBlog} from "../controllers/blogController.js"
+import {CreateBlog, getAllBlogs, getBlogsById, deleteBlogsById, updateBlogs} from "../controllers/blogController.js"
 const blogRoute = express.Router();
 
 blogRoute.post('/createBlog',upload.single('blogImage'),CreateBlog);
+blogRoute.get('/getAllBlogs',getAllBlogs);
+blogRoute.get('/getBlogById/:id',getBlogsById);
+blogRoute.delete('/deleteBlogById/:id',deleteBlogsById);
+blogRoute.put('/updateBlog/:id',updateBlogs);
 export default blogRoute;
